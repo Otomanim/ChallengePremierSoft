e89279970eb889104db0b88ea50cb73bca19345e
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2024-04-28 20:15:23 -0300

    ajust func get

--- DesafioPremierSoft/Scenes/NetworkService.swift
+++ DesafioPremierSoft/Scenes/NetworkService.swift
@@ -14,30 +14,40 @@ protocol NetworkServiceProtocol: AnyObject {
 class NetworkService: NetworkServiceProtocol {
     
     func getCandidates() -> [Candidate] {
-            var candidates: [Candidate] = []
-            if let url = URL(string: "https://raw.githubusercontent.com/premiersoft/AppAcademyChallenge2/main/AppAcademy_Candidates.csv") {
-                do {
-                    let contents = try String(contentsOf: url)
-                    let candidatesData = contents.split(whereSeparator: \.isNewline)
-                    var i = 0
-                    for candidateData in candidatesData {  // Separando cada candidato por linha
-                        let attributes = candidateData.split(separator: ";")  // Para cada candidato, separando seus atributos
-                        if i != 0 {  // Ignorando o cabeçalho
-                            let name = String(attributes[1])
-                            let vacancy = String(attributes[2])
-                            let age = String(attributes[3])
-                            let state = String(attributes[4])
-                            let candidate = Candidate(name: name, vacancy: vacancy, age: age, state: state)
-                            candidates.append(candidate)
-                        }
-                        i += 1
-                    }
-                } catch {
-                    print("Erro ao carregar dados: \(error)")
+        var separeteCandidates: [[String.SubSequence]] = [[]]
+        var arrayCandidates: [[String]] = []
+        var candidates: [Candidate] = []
+        if let url = URL(string: "https://raw.githubusercontent.com/premiersoft/AppAcademyChallenge2/main/AppAcademy_Candidates.csv") {
+            do {
+                let contents = try String(contentsOf: url)
+                let candidatesData = contents.split(whereSeparator: \.isNewline)
+                for candidateData in candidatesData {  // Separando cada candidato por linha
+                    let attributes = candidateData.split(separator: ";")  // Para cada candidato, separando seus atributos
+                    separeteCandidates.append(attributes)
                 }
-            } else {
-                print("URL inválida!")
+                var i = 0
+                for candidate in separeteCandidates {
+                    arrayCandidates.append(["Candidate \(i)"])
+                    for atribute in candidate {
+                        arrayCandidates[i].append(String(atribute))
+                    }
+                    i = i + 1
+                }
+                arrayCandidates.remove(at: 0)
+                for candidate in arrayCandidates {
+                    let name = candidate[1]
+                    let vacancy = candidate[2]
+                    let age = candidate[3]
+                    let state = candidate[4]
+                    let model = Candidate(Nome: name, Vaga: vacancy, Idade: age, Estado: state)
+                    candidates.append(model)
+                }
+            } catch {
+                print("Erro ao carregar dados: \(error)")
             }
-            return candidates
+        } else {
+            print("URL inválida!")
         }
+        return candidates
+    }
 }
