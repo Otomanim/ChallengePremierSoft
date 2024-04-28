65be9daea77ba451ad404dd7b5725713d38efcc7
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2024-04-28 20:14:05 -0300

    created and configure factory in project

--- DesafioPremierSoft/Scenes/HomeFactory.swift
+++ DesafioPremierSoft/Scenes/HomeFactory.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+enum HomeFactory {
+    static func makeModule() -> UIViewController {
+        let service = NetworkService()
+        let viewModel = HomeViewModel(networkService: service)
+        let viewController = HomeViewController(homeView: viewModel)
+        return viewController
+    }
+}
