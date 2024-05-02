56c6efef4bc89e96a551f1da9b2fca59269a6e93
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2024-05-02 16:02:38 -0300

    Created Cell at Candidate

--- DesafioPremierSoft/Scenes/Cell/CandidateCell.swift
+++ DesafioPremierSoft/Scenes/Cell/CandidateCell.swift
@@ -5,4 +5,94 @@
 //  Created by Evandro Rodrigo Minamoto on 23/04/24.
 //
 
-import Foundation
+import UIKit
+
+class CandidateCell: UITableViewCell {
+
+    private let candidateIsNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let candidateIsAgeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let candidateAtStateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let candidatesTypeOfVacancyLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var leftStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [candidateIsNameLabel, candidatesTypeOfVacancyLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 20
+        return stack
+    }()
+    
+    private lazy var rightStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [candidateIsAgeLabel, candidatesTypeOfVacancyLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 20
+        return stack
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [leftStackView, rightStackView])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.distribution = .fillProportionally
+        stack.spacing = 10
+        return stack
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        clear()
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func conigureLabels(with model: Candidate) {
+        candidateIsNameLabel.text = model.Nome
+        candidateIsAgeLabel.text = model.Idade
+        candidateAtStateLabel.text = model.Estado
+        candidatesTypeOfVacancyLabel.text = model.Vaga
+    }
+    
+    private func clear() {
+        candidateIsNameLabel.text = nil
+        candidateIsAgeLabel.text = nil
+        candidateAtStateLabel.text = nil
+        candidatesTypeOfVacancyLabel.text = nil
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackView.heightAnchor.constraint(equalToConstant: 80),
+            leftStackView.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.7),
+            rightStackView.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.3),
+            
+        ])
+    }
+}
